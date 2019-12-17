ed535bbd8dbd927e7825349e1b133e24ca86623f
ralph <rafal@creativeroom.pl>
2019-12-17 11:57:25 +0100

    stays bg

--- resources/views/components/stays.blade.php
+++ resources/views/components/stays.blade.php
@@ -1,60 +1,73 @@
-<section class="section section--color">
+<section class="section">
   <div class="stays">
     <h2>
       <span class="pretitle">
         Nasza oferta
       </span>
-      <span class="title">
+      <span class="title title--after">
         Pobyty
       </span>
     </h2>
-    <div class="stays__box">
-      <div class="stays__item">
-        <div class="stays__icon">
-          <img src="@asset("/images/stays_icon.png")" />
-        </div>
-        <div class="stays__desc">
-          <h3 class="text stays__title">
-            Pobyty lecznicze
-          </h3>
-          <p class="text stays__text">
-            Jest to jedna z metod - wanna / terapia osteopatycznych, stosuje się przy dolegliwościach związanych z ciałem pacjenta, ale również z całą jego osobą.
-          </p>
-          <a href="#" class="link">
-            Więcej &#8594;
-          </a>
+    <div class="stays__content">
+      <div class="stays__images">
+        <div class="stays__images-columns">
+          <div class="stays__images-left">
+            <img class="stays__img" src="@asset('images/p1.jpg')">
+            <img class="stays__img" src="@asset('images/p2.jpg')">
+          </div>
+          <div class="stays__images-right">
+            <img class="stays__img" src="@asset('images/p3.jpg')">
+          </div>
         </div>
       </div>
-      <div class="stays__item stays__item--rev">
-        <div class="stays__icon">
-          <img src="@asset("/images/stays_icon.png")" />
+      <div class="stays__box">
+        <div class="stays__item">
+          <div class="stays__icon">
+            <img src="@asset("/images/stays_icon.png")" />
+          </div>
+          <div class="stays__desc">
+            <h3 class="text stays__title">
+              Pobyty lecznicze
+            </h3>
+            <p class="text stays__text">
+              Jest to jedna z metod - wanna / terapia osteopatycznych, stosuje się przy dolegliwościach związanych z ciałem pacjenta, ale również z całą jego osobą.
+            </p>
+            <a href="#" class="link">
+              Więcej &#8594;
+            </a>
+          </div>
         </div>
-        <div class="stays__desc">
-          <h3 class="text stays__title">
-            Turnusy rehabilitacyjne
-          </h3>
-          <p class="text stays__text">
-            Zdjęcia z dziećmi Jest to jedna z metod osteopatycznych, stosuje się przy dolegliwościach związanych z ciałem pacjenta, ale również z całą jego osobą.
-          </p>
-          <a href="#" class="link">
-            Więcej &#8594;
-          </a>
+        <div class="stays__item stays__item--rev">
+          <div class="stays__icon">
+            <img src="@asset("/images/stays_icon.png")" />
+          </div>
+          <div class="stays__desc">
+            <h3 class="text stays__title">
+              Turnusy rehabilitacyjne
+            </h3>
+            <p class="text stays__text">
+              Zdjęcia z dziećmi Jest to jedna z metod osteopatycznych, stosuje się przy dolegliwościach związanych z ciałem pacjenta, ale również z całą jego osobą.
+            </p>
+            <a href="#" class="link">
+              Więcej &#8594;
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="stays__item">
-        <div class="stays__icon">
-          <img src="@asset("/images/stays_icon.png")" />
-        </div>
-        <div class="stays__desc">
-          <h3 class="text stays__title">
-            Pobyty wypocznykowe
-          </h3>
-          <p class="text stays__text">
-            Jest to jedna z metod - 2 dziadków wypoczywa osteopatycznych, stosuje się przy dolegliwościach związanych z ciałem pacjenta, ale również z całą jego osobą.
-          </p>
-          <a href="#" class="link">
-            Więcej &#8594;
-          </a>
+        <div class="stays__item">
+          <div class="stays__icon">
+            <img src="@asset("/images/stays_icon.png")" />
+          </div>
+          <div class="stays__desc">
+            <h3 class="text stays__title">
+              Pobyty wypocznykowe
+            </h3>
+            <p class="text stays__text">
+              Jest to jedna z metod - 2 dziadków wypoczywa osteopatycznych, stosuje się przy dolegliwościach związanych z ciałem pacjenta, ale również z całą jego osobą.
+            </p>
+            <a href="#" class="link">
+              Więcej &#8594;
+            </a>
+          </div>
         </div>
       </div>
     </div>
